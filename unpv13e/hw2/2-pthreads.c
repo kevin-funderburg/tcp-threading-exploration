81e44e2504cc9714c0555766f6c74109f199a595
kf1127 <kf1127@txstate.edu>
2021-06-24 17:25:54 -0500

    working on detaching

--- unpv13e/hw2/2-pthreads.c
+++ unpv13e/hw2/2-pthreads.c
@@ -11,13 +11,46 @@ void *doit(void *vargp)
     return NULL;
 }
 
-int main(int argc, char **argv)
+void *detach(void *vargp)
+{
+    sleep(1);
+    printf("new thread about to detach\n");
+    pthread_detach(vargp);
+       
+}
+
+int main(int argc, char *argv[])
 {
     pthread_t tid;
-    printf("main thread creating a new thread\n");
-    pthread_create(&tid, NULL, doit, NULL);
-    printf("main thread exiting\n");
-    pthread_exit(NULL);
-    printf("after thread\n");
-    exit(0);
+    int ret;
+
+    if (argc < 2) {
+        printf("error, expected 2 arguments\n");
+        return -1;
+    }
+
+    //test to see if a new thread can continue after creating
+    //thread exits using pthread_exit
+    if (strcmp(argv[1], "1") == 0) {
+        printf("main thread creating a new thread\n");
+        pthread_create(&tid, NULL, doit, NULL);
+        printf("main thread exiting\n");
+        pthread_exit(NULL);
+        printf("after thread\n");
+        exit(0);
+        return 0;
+    }
+    else if (strcmp(argv[1], "2") == 0) {
+        printf("main thread creating a new thread\n");
+        pthread_create(&tid, NULL, doit, NULL);
+        printf("main thread about to detach new thread\n");
+        ret = pthread_detach(tid);
+        if (ret) {
+            printf("ERROR: return code from pthread_detach() is %d\n", ret);
+            exit(-1);
+        }
+        //pthread_create(&tid, NULL, detach, NULL);
+    }
+        
+    
 }
