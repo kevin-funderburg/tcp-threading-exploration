f62abaea9fe341d98b65fb79859007d869112cef
kf1127 <kf1127@txstate.edu>
2021-06-28 15:19:59 -0500

    updated nagles

--- unpv13e/hw2/1-naglecli.c
+++ unpv13e/hw2/1-naglecli.c
@@ -3,10 +3,14 @@
 int
 main(int argc, char **argv)
 {
+    printf("...starting client...\n");
+
 	int					sockfd;
 	struct sockaddr_in	servaddr;
 
     char sendline[MAXLINE], recvline[MAXLINE];
+
+    FILE *file = fopen("input.txt", "r");
 
 	if (argc != 2)
 		err_quit("usage: tcpcli <IPaddress>");
@@ -20,15 +24,40 @@ main(int argc, char **argv)
 
 	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
-    int n = 0;
-    while ((sendline[n++] = getchar()) != '\n') //get text from command line
-        ;
+    printf("enter text to send to server, to quit type 'exit'\n");
 
-    Writen(sockfd, sendline, strlen(sendline));
+    int loop = 1;
+    while (loop)
+    {
+        bzero(sendline, MAXLINE);
+        int n = 0;
+        while ((sendline[n++] = getchar()) != '\n') //get text from command line
+            ;
 
-    if (Readline(sockfd, recvline, MAXLINE) == 0)
-        err_quit("str_cli: server terminated prematurely");
+        //printf("sendline: %s", sendline);
+        if (strncmp(sendline, "exit", 4) == 0) {
+            printf("exiting\n"); 
+            loop = 0;
+        }
+        Writen(sockfd, sendline, strlen(sendline));
 
-    printf("recvline is: %s\n", recvline);
-	exit(0);
+        if (Readline(sockfd, recvline, MAXLINE) == 0)
+            err_quit("str_cli: server terminated prematurely");
+
+        printf("recvline is: %s\n", recvline);
+        bzero(recvline, MAXLINE);
+    }
+    
+    //this method was to read from the input file and send to server, having some issue receiving though
+    //while(fscanf(file, "%[^\n] ", sendline) != EOF) {
+    //    printf("SENDING TO SERVER: %s\n", sendline); 
+    //    Writen(sockfd, sendline, strlen(sendline));
+
+    //    if (Readline(sockfd, recvline, MAXLINE) == 0)
+    //        err_quit("str_cli: server terminated prematurely");
+
+    //    printf("recvline is: %s\n", recvline);
+    //}	
+
+    exit(0);
 }
