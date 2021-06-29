9548f1b29c85a60c52ed8f3ad22046302eb4da27
kevin-funderburg <ksfunderburg@icloud.com>
2021-06-29 13:32:18 -0500

    moving str_echo to server code

--- unpv13e/hw2/3-tcpserv01.c
+++ unpv13e/hw2/3-tcpserv01.c
@@ -2,6 +2,22 @@
 
 static void	*doit(void *);		/* each thread executes this function */
 
+void
+str_echo(int sockfd)
+{
+	ssize_t		n;
+	char		buf[MAXLINE];
+
+again:
+	while ( (n = read(sockfd, buf, MAXLINE)) > 0)
+		Writen(sockfd, buf, n);
+
+	if (n < 0 && errno == EINTR)
+		goto again;
+	else if (n < 0)
+		err_sys("str_echo: read error");
+}
+
 int
 main(int argc, char **argv)
 {
